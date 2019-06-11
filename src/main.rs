0e6fa5a690e6155124519e06da2c264ae231d9b1
Martin Hagström <marhag87@gmail.com>
2019-06-11 13:27:05 +0200

    Use clap for argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use clap::{App, Arg};
 use ldiff::compare;
-use std::{env, fs};
+use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("ldiff")
+        .version("0.1.0")
+        .author("Martin Hagström")
+        .about("Compare two files line by line")
+        .arg(Arg::with_name("left").required(true))
+        .arg(Arg::with_name("right").required(true))
+        .get_matches();
 
-    let file_left = &args[1];
-    let file_right = &args[2];
-
+    let file_left = matches.value_of("left").unwrap();
+    let file_right = matches.value_of("right").unwrap();
     let left_content =
         fs::read_to_string(file_left).expect(&format!("Could not read file: {}", file_left));
     let right_content =
